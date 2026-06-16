4f58afd6d8111d4d0aecf0393f81767aa0a11a2c
John Doe <madda.athiarahman@gmail.com>
2026-06-16 21:26:19 +0700

    setup tanstack query

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import {
   Chart as ChartJS,
   CategoryScale,
@@ -17,6 +19,8 @@ import {
 import "./index.css";
 import App from "./routes/App.tsx";
 
+const queryClient = new QueryClient();
+
 ChartJS.register(
   CategoryScale,
   LinearScale,
@@ -32,8 +36,11 @@ ChartJS.register(
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   </StrictMode>,
 );
