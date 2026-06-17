7e4cda47b818eb92ed4d02012611c96ea54543eb
John Doe <madda.athiarahman@gmail.com>
2026-06-17 21:30:29 +0700

    add delete button on expense list

--- frontend/src/components/ui/ListExpense.tsx
+++ frontend/src/components/ui/ListExpense.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import type { ExpenseSchema } from "../../schemas/expenseSchema";
 import List from "./List";
+import { useDeleteExpense } from "../../hooks/useExpense";
 
 type Props = {
   datas: ExpenseSchema[];
 };
 
 const ListExpense = ({ datas }: Props) => {
+  const { mutate: deleteExpense } = useDeleteExpense();
+
   return (
     <>
       {datas.map((data) => {
@@ -17,6 +20,7 @@ const ListExpense = ({ datas }: Props) => {
             categoryName={data.category.name}
             date={data.date}
             amount={data.amount}
+            onDelete={() => deleteExpense(data._id as string)}
           />
         );
       })}
